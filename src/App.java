3b4ad2ee4a06276f699ba31131f5e5b7216913c3
Almno <anmol55@icloud.com>
2023-11-16 09:56:57 -0500

    Update App.java

--- src/App.java
+++ src/App.java
@@ -7,7 +7,7 @@ import java.io.FileWriter;
 public class App {
 
     public static void main(String[] args) throws Exception {
-        Wishing Wish = new Wishing(new Rates(60, 90, 74, 600), new Rates(510, 10, 10, 0));// honkais rates about
+        Wishing Wish = new Wishing(new Rates(60, 90, 75, 600), new Rates(510, 10, 10, 0));// honkais rates about
         Wish.ReadOutcomesFromCSV("WishingOutcomes.csv");
         int i = 0;
         BufferedWriter writer = new BufferedWriter(new FileWriter("result.csv"));
@@ -18,8 +18,10 @@ public class App {
             int a = Wish.FiveStarRates.currentPity;
             int b = Wish.FourStarRates.currentPity;
             thing aThing = Wish.summonThing();
-            writer.write(aThing.toCSV() + "," + a + "," + b + "\n");
-            if (i == 10000)
+            if (aThing.getRarity() == thing.Rarity.FiveStar || aThing.getRarity() == thing.Rarity.RateUpFiveStar)
+                writer.write(aThing.toCSV() + "," + a + "," + b + "\n");
+                
+            if (i == 100000)
                 break;
         }
         Wish.exportCSV();
